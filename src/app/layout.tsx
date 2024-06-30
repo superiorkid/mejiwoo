d6e7012f5556acb1d74150af7a7d2563ceaddd3a
superiorkid <bayiPetani@protonmail.com>
2024-06-30 21:25:52 +0800

    add shadcn-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -13,8 +14,15 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={GeistSans.className}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(
+					"min-h-screen antialiased bg-background",
+					GeistSans.className,
+				)}
+			>
+				{children}
+			</body>
 		</html>
 	);
 }
